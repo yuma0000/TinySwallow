0b31e49de77da224a2649d3b406bce910dcf4c98
yuma0000 <38005160+yuma0000@users.noreply.github.com>
2025-07-22 20:17:58 +0900

    バージョン 0.0.1

---
+++ src/init.py
@@ -0,0 +1,9 @@
+import nnabla as nnabla
+import nnabla.function as F
+from nnabla.function import PythonFunction
+
+import .tinyswallow_model
+
+
+def __init__():
+    print("start...\n")
---
+++ src/tinyswallow_model.py
@@ -0,0 +1,6 @@
+import nnabla as nnabla
+import nnabla.function as F
+from nnabla.function import PythonFunction
+
+class tinyswallow_model(PythonFunction):
+    
