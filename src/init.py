ba9b6a316b2d303a4b915afbef74de6f0cf1d5d0
yuma0000 <38005160+yuma0000@users.noreply.github.com>
2025-07-23 18:07:54 +0000

    0.0.3

--- src/init.py
+++ src/init.py
@@ -1,9 +1,23 @@
-import nnabla as nnabla
+import nnabla as nn
 import nnabla.function as F
 from nnabla.function import PythonFunction
 
+import .huggingface_model
 import .tinyswallow_model
+import .config
 
 
 def __init__():
+
+    #まずはhuggingface modelの取得をする
+    hfm = huggingface_model("/tinyswallow")
+
+    #tinyswallow_modelの構造
+    tsm = tinyswallow_model(hfm)
+    
+    #configの読み込み
+    cfg = config("config")
+
+    
+
     print("start...\n")
--- src/tinyswallow_model.py
+++ src/tinyswallow_model.py
@@ -1,6 +1,71 @@
-import nnabla as nnabla
-import nnabla.function as F
+import .nnabla_func
 from nnabla.function import PythonFunction
 
+class attention():
+    def __init__():
+        #Q, K, V, O
+
+    def forward():
+
+class rms_norm(PythonFunction):
+    def __init__():
+
+    def forward():
+
+class mlp():
+    def __init__():
+        #gate, up, down, act
+
+    def forward():
+
+class tokenizer(PythonFunction):
+    def __init__():
+
+    def forward():
+
+class embed_tokens(PythonFunction):
+    def __init__():
+
+    def forward():
+
+class decoder_layer(PythonFunction):
+    def __init__():
+        #self_attn
+        #mlp
+        #input_layernorm
+        #poat_attention_layernorm
+
+    def forward():
+
+class rotary_emb(PythonFunction):
+    def __init__():
+
+    def forward():
+
+class im_head(PythonFunction):
+    def __init__():
+
+    def forward():
+
 class tinyswallow_model(PythonFunction):
-    
+
+    def __init__():
+        #tokenizer
+        self.tokenizer = tokenizer()
+
+        #embed_tokens
+        self.embed_tokens = embed_tokens()
+
+        #layers 28
+        self.decoder_layer = decoder_layer()
+
+        #RMSnorm
+        self.rms_norm = rms_norm()
+
+        #rotary_emb
+        self.rotary_emb = rotary_emb()
+
+        #im_head
+        self.im_head = im_head()
+
+    def generate():
