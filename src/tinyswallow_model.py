6f187e3c178f9fb24965f529981a055a3bdc383f
yuma0000 <38005160+yuma0000@users.noreply.github.com>
2025-08-30 19:56:40 +0900

    Update tinyswallow_model.py

--- src/tinyswallow_model.py
+++ src/tinyswallow_model.py
@@ -1,28 +1,10 @@
-from .nnabla_func import *
-from nnabla.function import PythonFunction
+import os
+import json
+import nnabla_func as nf
 from functools import lru_cache
-
-class Attention():
-    def __init__(self):
-        #Q, K, V, O
-
-    def forward(self):
-
-class Rms_Norm(PythonFunction):
-    def __init__(self):
-
-    def forward(self):
-
-class Mlp():
-    def __init__(self):
-        #gate, up, down, act
-
-    def forward(self):
-
 
 class Tokenizer():
     def __init__(self, hfm):
-
         with open(os.path.join(hfm.path, hfm.files.vocab), "r", encoding="utf-8") as f:
             self.encoder = json.load(f)
         self.decoder = {v: k for k, v in self.encoder.items()}
@@ -36,11 +18,14 @@ class Tokenizer():
                     continue
                 bpe_merges.append(tuple(line.split()))
         self.bpe_ranks = dict(zip(bpe_merges, range(len(bpe_merges))))
-
         self.cache = {}
-        
+
+    def __call__(self, text):
+        bpe_tokens = self.bpe(text).split(" ")
+        token_ids = [self.encoder[token] for token in bpe_tokens if token in self.encoder]
+        return token_ids
     @lru_cache
-    def bytes_to_unicode():
+    def bytes_to_unicode(self):
         bs = (
             list(range(ord("!"), ord("~") + 1 )) + list(range(ord("¡"), ord("~") + 1)) + list(range(ord("@"), ord("y") + 1))
         )
@@ -54,7 +39,7 @@ class Tokenizer():
         cs = [chr(n) for n in cs]
         return dict(zip(bs, cs))
 
-    def get_pairs(word):
+    def get_pairs(self, word):
         pairs = set()
         prev_char = word[0]
         for char in word[1:]:
@@ -67,10 +52,8 @@ class Tokenizer():
             return self.cache[token]
         word = tuple(token)
         pairs = self.get_pairs(word)
-
         if not pairs:
             return token
-
         while True:
             bigram = min(pairs, key=lambda pair: self.bpe_ranks.get(pair, float("inf")))
             if bigram not in self.bpe_ranks:
@@ -95,56 +78,50 @@ class Tokenizer():
             if len(word) == 1:
                 break
             else:
-                pairs = get_pairs(word)
+                pairs = self.get_pairs(word)
         word = " ".join(word)
         self.cache[token] = word
         return word
 
-class Embed_tokens(PythonFunction):
-    def __init__(self):
-
-    def forward(self):
-
-class Decoder_layer(PythonFunction):
-    def __init__(self):
-        #self_attn
-        #mlp
-        #input_layernorm
-        #poat_attention_layernorm
-
-    def forward(self):
-
-class Rotary_emb(PythonFunction):
-    def __init__(self):
-
-    def forward(self):
-
-class Im_head(PythonFunction):
-    def __init__(self):
-
-    def forward(self):
-
-class Tinyswallow_model(PythonFunction):
-
+class Tinyswallow_model():
     def __init__(self, huggingface_model):
+        self.all_token_ids = []
+        self.state_dict = huggingface_model.state_dict
 
-        #tokenizer
+        self.config = None
+        self.cache = None
+        self.config_data = huggingface_model.config_data
+        self.bos_token = self.tokenizer.encoder.get("<BOS>")
+        self.eos_token = self.tokenizer.encoder.get("<EOS>")
+
         self.tokenizer = Tokenizer(huggingface_model)
-        print(self.tokenizer.bpe("hello world"))
+        print(self.tokenizer("hello world"))
 
-        #embed_tokens
-        self.embed_tokens = Embed_tokens()
+    def add_text(self, text):
+        token_ids = self.tokenizer(text)
+        self.all_token_ids.append(token_ids)
+        x = nf.embed_tokens(token_ids)
+        x = nf.rotary_emb(x)
+        self.decoder_layer.generate_kv(x)
 
-        #layers 28
-        self.decoder_layer = Decoder_layer()
+    def remove_text(self, c: int = 1):
+        self.all_token_ids = self.all_token_ids[:-c]
+        cache_data.remove_kv(c)
 
-        #RMSnorm
-        self.rms_norm = Rms_norm()
-
-        #rotary_emb
-        self.rotary_emb = Rotary_emb()
-
-        #im_head
-        self.im_head = Im_head()
-
-    def generate(self):
+    def generate(self, max_length: int = 1):
+        token_ids = [self.bos_token]
+        x = [self.bos_token]
+        for _ in range(max_length):
+            x = nf.embed_tokens(x)
+            x = nf.decoder_layer(self.config_data.max_layer, x)
+            if last_token == self.eos_token:
+                break
+            x = nf.rms_norm(state_dict[f"model.norm.weight"], x)
+            logits = nf.im_head(x)
+            last_token = int(logits[0].argmax())
+            token_ids.append(last_token)
+            x = [last_token]
+        self.all_token_ids.append(token_ids)
+        tokens = [self.tokenizer.decoder[t] for t in token_ids]
+        text_out = "".join(tokens)
+        return text_out
