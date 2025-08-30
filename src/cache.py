c2228dc9d213682c7f32a259bb37bfa77e92c8b3
yuma0000 <38005160+yuma0000@users.noreply.github.com>
2025-08-30 19:58:11 +0900

    Update cache.py

---
+++ src/cache.py
@@ -0,0 +1,17 @@
+class cache:
+    def __init__(path: str):
+        self.k = []
+        self.v = []
+
+    def search():
+
+    def get():
+
+    def save():
+
+    def add_kv(k, v):
+        self.k = F.concatenate(self.k, k, axis=2)
+        self.v = F.concatenate(self.v, v, axis=2)
+        return self.k, self.v
+
+    def delete_kv:
