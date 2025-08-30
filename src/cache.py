f47652482576a29b763e780e49cb4a8bd18f65b5
yuma0000 <38005160+yuma0000@users.noreply.github.com>
2025-08-30 21:27:04 +0900

    Update cache.py

--- src/cache.py
+++ src/cache.py
@@ -14,4 +14,6 @@ class cache:
         self.v = F.concatenate(self.v, v, axis=2)
         return self.k, self.v
 
-    def delete_kv:
+    def delete_kv(c):
+        self.k = F.slice(self.k, start=(0,0,0,0), stop=(self.k.shape[0], self.k.shape[1], self.k.shape[2] - c, self.k.shape[3]))
+        self.v = F.slice(self.v, start=(0,0,0,0), stop=(self.v.shape[0], self.v.shape[1], self.v.shape[2] - c, self.v.shape[3]))
